1037d88a6e69af61f350f6dd5c429a6e7c13c0fa
Glitch (glitch-hello-node) <none>
2022-06-20 04:33:58 +0000

    🍫🚀 Checkpoint ./server.js:1386568/1126

--- server.js
+++ server.js
@@ -89,12 +89,15 @@ fastify.get("/test", function (request, reply) {
     maxRedirects: 0,
     headers: {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "authority": "vt.tiktok.com",
+        ":authority:": "www.tiktok.com",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "vi,en;q=0.9",
         "Connection": "keep-alive",
-        "Host": " vt.tiktok.com",
+        "Host": "www.tiktok.com",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "referer": "www.tiktok.com",
     }
   }).then((res) => {
     reply.send(res.data);
