8209761c06679c55ba3c95ef74a5dae7a0802a4f
Glitch (glitch-hello-node) <none>
2022-06-20 05:07:30 +0000

    🙅✌️ Checkpoint ./server.js:1386568/2

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ fastify.get("/test", function (request, reply) {
     maxRedirects: 0,
     headers: {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        ":authority:": "www.tiktok.com",
+        "authority": "www.tiktok.com",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "vi,en;q=0.9",
         "Connection": "keep-alive",
