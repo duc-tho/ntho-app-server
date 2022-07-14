9906385f248ec1c3da87637cc726164d6c9f8244
Glitch (ndt-tt) <none>
2022-07-14 09:37:41 +0000

    📹🗼 Checkpoint ./src/core/db.js:1386568/1252

--- src/core/db.js
+++ src/core/db.js
@@ -1,12 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-// TODO: Replace the following with your app's Firebase project configuration
-// See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
   databaseURL: "https://DATABASE_NAME.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID",
 };
 
 // Initialize Firebase
@@ -15,3 +18,5 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
+
+export default database;
