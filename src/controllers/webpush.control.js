4c3c658f9a14cdcd8e6900c96afe49ca61284176
Glitch (ndt-tt) <none>
2022-07-20 10:53:28 +0000

    🚒🍟 Checkpoint ./src/controllers/webpush.control.js:1386568/4507

--- src/controllers/webpush.control.js
+++ src/controllers/webpush.control.js
@@ -1,28 +1,51 @@
-const { db } = require('../core/db');
+const { db } = require("../core/db");
 
 exports.webpushController = {
   push: (req, rep) => {
-    db.get('pushTokens').then(data => {
-        let deviceTokens = data.val();
-      
-        console.log(deviceTokens);
-      
-    });
-    
-    rep.send("pút");
-  },
-  
-  saveDeviceToken: (req, rep) => {
-    if (!req.body.deviceToken) rep.send({
-          success: false,
-          reason: 'Không có token sao mà lưu :<',
-     });
-    
-    db.set('pushTokens', req.body.deviceToken);
-    
-    rep.send({
+    db.get("pushTokens").then((data) => {
+      let deviceTokens = Object.values(data.val());
+
+      if (deviceTokens.length <= 0)
+        rep.send({
           success: true,
-          reason: 'Lưu thành công',
-     });
-  }
-}
+          reason: "Chưa có thiết bị nào",
+        });
+
+      const finalDeviceTokens = deviceTokens.filter(
+        (deviceToken, index, self) => self.indexOf(deviceToken) === index
+      );
+
+      console.log(finalDeviceTokens);
+
+      rep.send({
+        success: true,
+        reason: "Send success thành công",
+      });
+    });
+  },
+
+  saveDeviceToken: (req, rep) => {
+    if (!req.body.deviceToken)
+      rep.send({
+        success: false,
+        reason: "Không có token sao mà lưu :<",
+      });
+
+    db.get("pushTokens").then((data) => {
+      let deviceTokens = Object.values(data.val());
+
+      if (deviceTokens.includes(req.body.deviceToken))
+        rep.send({
+          success: false,
+          reason: "Token đã tồn tại!",
+        });
+
+      db.set("pushTokens", req.body.deviceToken);
+
+      rep.send({
+        success: true,
+        reason: "Lưu thành công",
+      });
+    });
+  },
+};
