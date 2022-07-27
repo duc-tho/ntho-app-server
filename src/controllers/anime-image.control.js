b8fcbd78ad96932b5dd50525c6339da3d1c48cd5
Glitch (ndt-tt) <none>
2022-07-27 10:39:02 +0000

    🐵🎟 Checkpoint ./src/controllers/anime-image.control.js:1386568/3368

--- src/controllers/anime-image.control.js
+++ src/controllers/anime-image.control.js
@@ -1,7 +1,46 @@
-const akaneko = require('akaneko');
+const akaneko = require("akaneko");
 
 exports.animeImageController = async (req, rep) => {
-    let url = await akaneko.nsfw.cum();
-    
-    rep.send(url);
-}
+  let url = await akaneko.nsfw.cum();
+
+  let sfw = ["neko", "lewdNeko", "lewdBomb", "wallpapers", "mobileWallpapers"];
+
+  let nsfw = [
+    "cum",
+    "doujin",
+    "femdom",
+    "hentai",
+    "maid",
+    "maids",
+    "orgy",
+    "panties",
+    "wallpapers",
+    "mobileWallpapers",
+    "cuckold",
+    "netorare",
+    "gifs",
+    "gif",
+    "blowjob",
+    "feet",
+    "pussy",
+    "uglyBastard",
+    "uniform",
+    "gangbang",
+    "foxgirl",
+    "cumslut",
+    "glasses",
+    "thighs",
+    "tentacles",
+    "masturbation",
+    "school",
+    "yuri",
+    "zettaiRyouiki",
+    "succubus",
+  ];
+
+  let random = Math.floor(Math.random() * 36);
+  let type = sfw.concat(nsf)
+  
+  
+  rep.send(url);
+};
