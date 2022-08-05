9291d746ac8e04df248014ca8990316bf13b6279
Glitch (ndt-tt) <none>
2022-08-05 11:08:45 +0000

    db

--- src/models/tiktokhistory.js
+++ src/models/tiktokhistory.js
@@ -1,23 +1,27 @@
-"use strict";
-const { Model } = require("sequelize");
+'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class TiktokHistory extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
     static associate(models) {
       // define association here
     }
   }
-  TiktokHistory.init(
-    {
-      id: DataTypes.UUID,
-      title: DataTypes.STRING,
-      nwm: DataTypes.STRING,
-      wm: DataTypes.STRING,
-      url: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "TiktokHistory",
-    }
-  );
+  TiktokHistory.init({
+    id: DataTypes.UUID,
+    title: DataTypes.STRING,
+    nwm: DataTypes.STRING,
+    wm: DataTypes.STRING,
+    url: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'TiktokHistory',
+  });
   return TiktokHistory;
 };
